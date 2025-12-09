2b35d260fd5f6ea4af71e1a64cd27a256d2ce493
Marcos Fonseca <marcosf_@hotmail.com>
2025-12-09 11:53:32 -0300

    Fixed Registrar not found symbol issue

--- android/src/main/java/co/creativemind/device_information/DeviceInformationPlugin.java
+++ android/src/main/java/co/creativemind/device_information/DeviceInformationPlugin.java
@@ -24,7 +24,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** DeviceInformationPlugin */
 public class DeviceInformationPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
